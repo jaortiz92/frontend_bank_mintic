f62bc1a0056223a420f050595ab89dadbac1e8fa
John Ortiz <jaortiz92@hotmail.com>
2021-12-02 09:18:56 -0500

    Modificacion de clase, Configuracion de apollo y componente signup

--- ciclo4/bank_fe/src/main.js
+++ ciclo4/bank_fe/src/main.js
@@ -1,5 +1,30 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import { ApolloClient, createHttpLink, InMemoryCache } from '@apollo/client/core'
+import { createApolloPovider } from '@vue/apollo-option'
+import { setContext } from 'apollo-link-context'
 
-createApp(App).use(router).mount('#app')
+const httplink = createHttpLink({
+    uri: 'https://mt-c4-104462-apigateway.herokuapp.com/',
+})
+
+const authLink = setContext((_, { headers }) => {
+    return {
+        headers: {
+            ...headers,
+            'Authorization': localStorage.getItem('token_access') || ""
+        }
+    }
+})
+
+const apolloClient = new ApolloClient({
+    link: authLink.concat(httplink),
+    cache: new InMemoryCache()
+})
+
+const apolloProvider = new createApolloPovider({
+    defaultClient: apolloClient
+})
+
+createApp(App).use(router).use(apolloProvider).mount('#app')
